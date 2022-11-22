c598c1dc54e04e076f77e928975726e73348fc55
iamdaeun <coekdms0426@gmail.com>
2022-11-22 12:45:54 +0900

    feat: add loading

--- app/src/main/java/com/example/capstone/RecordActivity.java
+++ app/src/main/java/com/example/capstone/RecordActivity.java
@@ -188,9 +188,6 @@ public class RecordActivity extends AppCompatActivity {
                     //Log.w("client", "버퍼 생성 성공");
 
                     try {
-                        Message hold_message = loadHandler.obtainMessage();
-                        loadHandler.sendMessage(hold_message);
-
                         while ((byteBuffer.position() + SAMPLING_RATE_IN_HZ) < BUFFER_BYTE_SIZE) {
                             retBufferSize = audioRecord.read(BufferRecord, 0, SAMPLING_RATE_IN_HZ);
                             byteBuffer.put(BufferRecord, 0, retBufferSize);
@@ -199,6 +196,9 @@ public class RecordActivity extends AppCompatActivity {
                             dos.flush();
                         }
                         //Log.w("client", "전송 완료");
+
+                        Message load_message = loadHandler.obtainMessage();
+                        loadHandler.sendMessage(load_message);
 
                         byte[] buf = new byte[512];
                         //Log.w("client", "수신 대기 중");
@@ -214,7 +214,6 @@ public class RecordActivity extends AppCompatActivity {
                     audioRecord.stop();
                     audioRecord.release();
 
-                    input_message = "positive 90";
                     Message msg = handler.obtainMessage();
                     handler.sendMessage(msg);
                 }
